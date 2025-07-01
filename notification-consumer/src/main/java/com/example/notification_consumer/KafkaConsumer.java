8552137d1d4c1a352712e00eff08ffc4083b6f0b
habib-wahid <habib11wahid@gmail.com>
2025-07-01 11:19:06 +0600

    basic bulk consumer added

--- notification-consumer/src/main/java/com/example/notification_consumer/KafkaConsumer.java
+++ notification-consumer/src/main/java/com/example/notification_consumer/KafkaConsumer.java
@@ -1,5 +1,7 @@
 package com.example.notification_consumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -21,6 +23,18 @@ public class KafkaConsumer {
             System.out.println(notificationEvent.getNotificationType());
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @KafkaListener(topics = "bulk-user-notification", groupId = "bulk-group")
+    public void bulkConsume(String event) {
+        System.out.println("Received event: " + event);
+        try {
+            NotificationEvent notificationEvent =
+                    objectMapper.readValue(event, NotificationEvent.class);
+            System.out.println(notificationEvent.getUserId() + " " + notificationEvent.getNotificationType());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
